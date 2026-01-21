bd1b385a2936a94a39473e90007e08bc55cf21d3
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2026-01-21 22:32:14 +0600

    update : implement 404 not fount middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { toNodeHandler } from "better-auth/node";
 import cors from 'cors';
 import { commentRouter } from './modules/comment/comment.router';
 import errorHandler from './middlewares/globalErrorHandler';
+import { notFound } from './middlewares/notFound';
 
 
 const app = express();
@@ -27,6 +28,8 @@ app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
 
 // Error Handler   
+
+app.use(notFound)
 app.use(errorHandler)
 
 export default app;
